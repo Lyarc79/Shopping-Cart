323cce1b00b4760edf13695f9a8ee8db4a401adb
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-04-15 16:42:01 +0200

    Add error handling

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -7,30 +7,49 @@ export default function Shop() {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { handleAddToCart } = useOutletContext();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setProducts(data);
-      setIsLoading(false);
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) throw new Error("API_ERROR");
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        const message =
+          err.message === "API_ERROR"
+            ? "Server is down"
+            : "Check your connection (or VPN!)";
+        setError(message);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchProducts();
   }, []);
 
-  if (isLoading) return <h2>Loading products...</h2>;
-
   return (
-    <div className={styles.shopGrid}>
-      {products.map((product) => {
-        return (
-          <ProductCard
-            key={product.id}
-            product={product}
-            handleAddToCart={handleAddToCart}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div>
+        {isLoading && <p>Loading products...</p>}
+        {error && (
+          <div>
+            <p>{error}</p>
+          </div>
+        )}
+      </div>
+      <div className={styles.shopGrid}>
+        {products.map((product) => {
+          return (
+            <ProductCard
+              key={product.id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            />
+          );
+        })}
+      </div>
+    </>
   );
 }
